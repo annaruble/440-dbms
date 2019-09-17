fc7d8095ca453c939483f76e7ecd601e200774cb
Anna Ruble <rublan01@annas-mbp.lc.luther.edu>
2019-09-16 21:03:22 -0500

    adding JsonProperty values

--- src/mlb/Team.java
+++ src/mlb/Team.java
@@ -3,15 +3,21 @@ package mlb;
  * @author Roman Yasinovskyy
  */
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.ArrayList;
 @JsonIgnoreProperties(ignoreUnknown = true)
 
 public class Team {
     // TODO: Use JsonProperty correctly
+    @JsonProperty("id")
     private final String id;
+    @JsonProperty("abbreviation")
     private final String abbreviation;
+    @JsonProperty("name")
     private final String name;
+    @JsonProperty("conference")
     private final String conference;
+    @JsonProperty("division")
     private final String division;
     private ArrayList<Player> roster;
     private Address address;
