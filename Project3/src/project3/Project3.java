8edb2cf93a0bd1a00b304a62c47cfbcc99c0ff4d
Anna Ruble <rublan01@annas-mbp.lc.luther.edu>
2019-10-29 21:23:22 -0500

    add trigger

--- Project3/src/project3/Project3.java
+++ Project3/src/project3/Project3.java
@@ -8,6 +8,9 @@ import com.opencsv.exceptions.CsvValidationException;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import static project3.Views.createConnection;
 
 /**
  *
@@ -21,8 +24,58 @@ public class Project3 {
      * @param args the command line arguments
      */
   
+    static void createTrigger() {
+        Connection conn = createConnection();
+        Statement stmt;
+        
+        //New faculty have endDt set to null
+        String trigger = "USE UniversityDB;\n"
+                + "CREATE TRIGGER `newFaculty`\n"
+                + "AFTER INSERT\n"
+                + "ON faculty FOR EACH ROW\n" +
+            "BEGIN\n" +
+            "SET NEW.endDate = NULL;";
+
+
+        try {
+            stmt = conn.createStatement();
+            stmt.executeUpdate(trigger);
+            
+            System.out.println("new faculty have end date null...thanks trigger!");
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(Project3.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    static void createTrigger2() {
+        Connection conn = createConnection();
+        Statement stmt;
+        
+        //New student has major set to null
+        String trigger = "USE UniversityDB;\n"
+                + "CREATE TRIGGER `newStudent`\n"
+                + "AFTER INSERT\n"
+                + "ON STUDENT FOR EACH ROW\n" +
+            "BEGIN\n" +
+            "SET NEW.major = NULL;";
+
+
+        try {
+            stmt = conn.createStatement();
+            stmt.executeUpdate(trigger);
+            
+            System.out.println("new students have major as null...thanks trigger!");
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(Project3.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
     public static void main(String[] args) throws IOException, FileNotFoundException, CsvValidationException {
         CreateTable.createAll();   
+        //createTrigger();
+        //createTrigger2();
         InsertData.insertAll();
         Views.createSeniorView();
         Views.createCurrentCoursesView();
